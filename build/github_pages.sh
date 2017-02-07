446c8d1a45f3b58441f23f5ad59a9f8ce0dac391
koolkat <flores_a@epitech.eu>
2017-02-07 02:11:07 +0100

    github pages

---
+++ build/github_pages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+build_path="resources/public/js/compiled/cmap.js"
+
+rm -rf docs/*
+lein trampoline cljsbuild once min
+cp -r resources/public/{js,index.html,css} docs/
+md5=($(md5sum "$build_path"))
+build_hash=${md5:0:9}
+
+sed -i "s/cmap.js/cmap-$build_hash.js/g" docs/index.html
+sed -i "s/style.css/style-$build_hash.css/g" docs/index.html
+mv docs/js/compiled/cmap.js docs/js/compiled/cmap-$build_hash.js
+mv docs/css/style.css docs/css/style-$build_hash.css
