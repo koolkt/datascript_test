3c8bbad492441d537e5185a95c725736b9fa3dec
koolkat <flores_a@epitech.eu>
2017-02-07 03:00:44 +0100

    build github page

--- build/github_pages.sh
+++ build/github_pages.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-build_path="resources/public/js/compiled/cmap.js"
+js_build_path="docs/js/compiled/cmap-prod.js"
+css_build_path="docs/css/style.css"
 
 rm -rf docs/*
 lein trampoline cljsbuild once min
 cp -r resources/public/{js,index.html,css} docs/
-md5=($(md5sum "$build_path"))
-build_hash=${md5:0:9}
+js_md5=($(md5sum "$js_build_path"))
+css_md5=($(md5sum "$css_build_path"))
+js_build_hash=${js_md5:0:9}
+css_build_hash=${css_md5:0:9}
 
-sed -i "s/cmap.js/cmap-$build_hash.js/g" docs/index.html
-sed -i "s/style.css/style-$build_hash.css/g" docs/index.html
-mv docs/js/compiled/cmap-prod.js docs/js/compiled/cmap-$build_hash.js
-mv docs/css/style.css docs/css/style-$build_hash.css
-rm docs/js/compiled/cmap.js
+sed -i "s/cmap.js/cmap-$js_build_hash.js/g" docs/index.html
+sed -i "s/style.css/style-$css_build_hash.css/g" docs/index.html
+mv docs/js/compiled/cmap-prod.js docs/js/compiled/cmap-$js_build_hash.js
+mv docs/css/style.css docs/css/style-$css_build_hash.css
+rm -rf docs/js/compiled/cmap.js
